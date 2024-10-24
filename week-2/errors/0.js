1c749e301eced94e9930d99b0b88dd95faf9e085
Said Akbarov <134784793+FrenchFry36@users.noreply.github.com>
2024-10-24 16:39:22 +0200

    errors resolved

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -1,9 +1,10 @@
 // Predict and explain first...
-// write down the error you predict will be raised
+// write down the error you predict will be raised - already declared str variable
 // then call the function capitalise with a string input
 // interpret the error message and figure out why it's happening, if your prediction was wrong
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
+
+console.log(capitalise("human"));
--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -1,13 +1,13 @@
 // Predict and explain first...
-// Write down the error you predict will be raised
+// Write down the error you predict will be raised -
 // Why will an error occur when this program runs?
 // Play computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage());
